6a67fa26569a01e7e4e6e536446a440445794b16
Arthur <arthur.silva@aluno.faculdadeimpacta.com.br>
2021-02-03 12:57:08 -0300

    Impacta_ED - Java
    Tarefas: 1, 2 e 3

---
+++ Impacta/Tarefas/IDE/workspace/UsandoJUnit1/src/calculadora/Calculadora.java
@@ -0,0 +1,24 @@
+package calculadora;
+
+public class Calculadora {
+
+	public double soma(double d, double e) {
+		return d + e;
+	}
+
+	public double sub(double d, double e) {
+		return d - e;
+	}
+
+	public double mult(double d, double e) {
+		return d * e;
+	}
+
+	public double div(double d, double e) {
+		return d / e;
+	}
+
+	public Double potenc(double d) {
+		return d * d;
+	}
+}
